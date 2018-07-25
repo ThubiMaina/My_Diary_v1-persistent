6125a25fd3ad4c0b8a449cf5127dda272eb8ee1c
ThubiMaina <erickmaina35@gmail.com>
2018-07-25 09:38:57 +0300

    set up the required data before testing

---
+++ tests/test_entry.py
@@ -0,0 +1,39 @@
+import unittest
+import json
+import sys
+import os
+import inspect
+
+currentdir = os.path.dirname(
+
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+parentdir = os.path.dirname(currentdir)
+
+sys.path.insert(0, parentdir)
+from app.app import create_app
+
+class EntryTestCase(unittest.TestCase):
+    """Test case for the Diary entries"""
+
+    def setUp(self):
+        """Set up test variables."""
+        self.app = create_app(config_name='testing').test_client()
+        self.entry_data = json.dumps(dict({
+                    "owner": "erick",
+                    "title": "A day in space"
+                }))
+        self.login_data = json.dumps(dict({
+            "email": "erick@gmail.com",
+            "password": "password"
+        }))
+        self.register_data = json.dumps(dict({
+            "username":"erick",
+            "email":"erick@gmail.com",
+            "password":"password"
+            }))
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
