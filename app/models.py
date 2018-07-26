3795163872dabed35f49de9b05792e4b8904cab2
ThubiMaina <erickmaina35@gmail.com>
2018-07-26 10:19:56 +0300

    a method to encode and decode a token using the email

--- app/models.py
+++ app/models.py
@@ -12,4 +12,30 @@ class User:
         self.email = email
         self.password = password
 
+    @staticmethod
+    def generate_token(email):
+        try:
+            payload = {
+            'iss': "mydiary",
+            'iat': datetime.utcnow(),
+            'exp': datetime.utcnow() + timedelta(minutes=30),
+            'sub': email
+            }
+            jwt_string = jwt.encode(payload,
+             'secret', algorithm='HS256')
+            return jwt_string.decode("utf-8")
+        except Exception as e:
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        """Decodes the access token from the authorization header"""
+        try:
+            payload = jwt.decode(token, 'secret')
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return {"error": "Expired token. Please login to get a new token"}
+        except jwt.InvalidTokenError:
+            return {"error":"Invalid token. Please register or login"}
+
     
