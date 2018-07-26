b73f06ad593d260cd43d0f446a6ffb0abd0f932f
ThubiMaina <erickmaina35@gmail.com>
2018-07-26 10:32:36 +0300

    User register endpoint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,76 @@
 # app/__init__.py
 
+import re
+from datetime import datetime
 from flask_api import FlaskAPI
+from flask import request, jsonify, abort
+from app import models
+from .models import *
+from app.auth import auth_token
+
+# local import
 from instance.config import app_config
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config["development"])
     app.config.from_pyfile('config.py')
+
+    @app.route('/api/auth/register/', methods=['POST'])
+    def create_user():
+        """api endpoint to create a new user"""
+        if request.method == 'POST':
+            data = request.get_json()
+            email = data.get('email')
+            username = data.get('username')
+            password = data.get('password')
+            admin = data.pop('admin', False)
+            regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+            if email == "":
+                response = jsonify({'error': 'email field cannot be blank'})
+                response.status_code = 400
+                return response
+
+            elif not re.match(regex, email):
+                response = jsonify({
+                    'error':'try entering a valid email'
+                })
+                response.status_code = 403
+                return response
+
+            elif username == "":
+                response = jsonify({'error' : 'username field cannot be blank'})
+                response.status_code = 400
+                return response
+
+            elif not re.match("^[a-zA-Z0-9_]*$", username):
+                response = jsonify({'error':
+                                    'Username cannot contain special characters'})
+                response.status_code = 403
+                return response
+
+            elif password == "":
+                response = jsonify({'error':'password field has to be filled'})
+                response.status_code = 400
+                return response
+            elif len(password) < 6:#pylint:disable=C1801
+                response = jsonify({'error':
+                                    'Password should be more than 6 characters'})
+                response.status_code = 403
+                return response
+                
+            userslist = {
+                'username': username,
+                'email': email,
+                'password': password,
+                }
+            u = User(**userslist)
+            u.save()
+            response = jsonify({'message': 'welcome you now can log in'})
+            #users created successfully
+            response.status_code = 201
+            return response
+
+    
+
     return app
